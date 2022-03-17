d35c5e96d2adcde5564b480f391891cd49d23cd5
ytendx <endx@web.de>
2022-03-17 21:03:57 +0100

    Improved Design and fixed false flag

--- API.js
+++ API.js
@@ -1,17 +1,23 @@
 const apiAddress = "dev.aunacraft.de:8082"
 
+var currentlyLoadedID = "Nothing is loaded!";
+
 async function load(logID) {
+
+    document.title = "Loading..."
+
     const response = await fetch("http://" + apiAddress + "/chatlog/v1/get/" + logID + "/")
 
-    console.log("1 - " + logID)
+    currentlyLoadedID = logID
 
     if(response.ok) {
+
+        document.title = "AunaCraft | ChatLog#" + logID
+
         const json = await response.json();
         const messages = json["messages"]
 
         const table = document.getElementById('table');
-
-        console.log(2)
 
         for (let i = 0; i < messages.length; i++) {
             const messageObject = messages[i]
@@ -28,17 +34,23 @@ async function load(logID) {
 							<td id="time">${dateObject.toLocaleString("de-DE")}</td>
 					  </tr>`
             table.innerHTML += row
-
         }
     }else {
+
+        document.title = "AunaCraft | ChatLog [Error]"
+
         var errorMessage = "Response-Code: " + response.status;
         if(response.status == 404){
             errorMessage = "Requested Log does not exist"
         }
         setState(`<h1 style="color: #dd0027">Error</h1>
-         <h3>${errorMessage}</h3>`
+         <h3 style="padding-bottom: 400px">${errorMessage}</h3>`
         )
     }
+}
+
+function delay(time) {
+    return new Promise(resolve => setTimeout(resolve, time));
 }
 
 function resetTable(){
@@ -49,7 +61,14 @@ function resetTable(){
 
 document.getElementById("searchButton").addEventListener("click", function () {
     const logID = document.querySelector("input").value;
-    console.log(logID)
+
+    if(logID == currentlyLoadedID){
+        setState("<h3>This Log is already shown!</h3>")
+        delay(1000).then(() => setState(""));
+        return
+    }
+
+    console.log("Showing log with id " + logID)
     resetTable()
     load(logID)
 })
@@ -72,7 +91,7 @@ if(logID != null) {
 }else {
     setState(
         `<h1>Invalid Request</h1>
-         <h3>URL-Parameter logID is missing</h3>`
+         <h3 style="padding-bottom: 400px">URL-Parameter logID is missing</h3>`
     )
 }
 
